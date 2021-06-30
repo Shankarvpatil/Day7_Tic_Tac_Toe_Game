605aaab4992dea60f31c1dfba78d6958c516e0ae
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-30 18:16:58 +0530

    Update Tic_Tac_Toe.java

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,15 +1,33 @@
+
 package com.bridgelabz.workshop.tictactoe;
+
+import java.util.Scanner;
 
 public class TicTacToeGame {
 	static char[] board = new char[10];
+	static char player,computer;
+	public static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe");
-		
+		getBoard();  // call for board
+		getPlayer();  // call for option
 	}
-	static void getBoard() {
-        for(int i=0; i<10; i++) {
-            board[i]=' ';
-        }
-    }
+	
+	// method for board 
+	 static void getBoard() {
+		 for (int i = 0; i < 10; i++) {
+			 board[i] = '-';
+		 }
+	 }
+	 
+	 // method to choose the option
+	 static void getPlayer() {
+		 System.out.print("Choose an Option x or o : ");
+		 player = scanner.next().charAt(0);
+		 if (player == 'x')
+			 computer='o';
+		 else
+			 computer ='x';
+		 System.out.println("You Selected : " +player);
+	    }
 }
