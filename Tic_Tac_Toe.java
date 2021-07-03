a6c4ea253d5eb44b27d8151fad872bdf914d76e1
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-07-03 07:46:48 +0530

    Update Tic_Tac_Toe.java

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -6,7 +6,7 @@ public class TicTacTaoGame {
 	Scanner scanner;
 	static char[] board = new char[10];
 	boolean empty = true;
-	static String player, computer, playersign, computersign;
+	static String playername, computername, playersign, compsign;
 
 	public TicTacTaoGame() {
 		scanner = new Scanner(System.in);
